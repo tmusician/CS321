f3d58abf040a9eb7035fc3b7e906a2647e5be19a
Sean Gamble <seanmgamble@aim.com>
2016-03-29 19:31:15 -0400

    stuffs
    s

--- Assignment4/SoftwareArchitecturalDesign/src/Rooms.java
+++ Assignment4/SoftwareArchitecturalDesign/src/Rooms.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
 
+//hold what rooms are in use
 public class Rooms {
+	//list of rooms, location in array goes to room #. Double is location+size of singles 
+	private static ArrayList<Integer> sigRoomsInUse = new ArrayList<Integer>();
+	private static ArrayList<Integer> doubRoomsInUse = new ArrayList<Integer>();
+	
+	public static void init(){
+		int numSig=Framework.NUM_SINGLE_ROOMS;
+		int numDob=Framework.NUM_DOUBLE_ROOMS;
+		//set all to 0, meaning empty
+		for(int i=0; i<numSig;i++){
+			sigRoomsInUse.add(0);
+		}
+		for(int x=0; x<numDob;x++){
+			doubleRoomsInUse.add(0);
+		}
+	}
+	
+	public static void fillRoom(int roomNum) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	public static int getEmpty(int roomType) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
