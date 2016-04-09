4feb4d66494fdab3b4ff38ecbd1e6088c0f06ae9
Airek64 <emille29@gmu.edu>
2016-04-09 19:26:04 -0400

    Added Day
    Day class added, and checkIn updated (still needs output

--- Assignment4/SoftwareArchitecturalDesign/src/CheckIn.java
+++ Assignment4/SoftwareArchitecturalDesign/src/CheckIn.java
@@ -1,16 +1,15 @@
-
-
+// business logic class used to check in a customer 
 public class CheckIn {
 
-	public static void run(String[] instr) {
-		// TODO Auto-generated method stub
-		Customer cust=Framework.getCustomerByName(instr[1]);
-		Reservation userRes= Framework.getReservationByCID(cust.getCustomerID());
-		boolean checkedIn = false;
+	// checks in customer dictated by an instruction list
+	public static void checkIn(String[] instr) {
+		// initialize variables 
+		Customer cust=Framework.getCustomerByName(instr[1]); // the customer data retrieved by customer's name
+		Reservation userRes= Framework.getReservationByCID(cust.getCustomerID()); // the reservation for the customer retrieved by customer ID
+		boolean checkedIn = false; // keeps track if customer has necessary information to check in
 		
-		if (instr[2] != null){
-			userRes.setStatus(2);
-			
+		// if a credit type is given then update the credit card information 
+		if (instr.length == 5){				// if instruction does not include 5 arguemtns, then no credit type was given (it's the only option value
 			cust.setCCType(instr[2]);
 			cust.setCCExpiration(instr[3]);
 			cust.setCCNumber(instr[4]);
@@ -18,19 +17,20 @@ public class CheckIn {
 			checkedIn = true;
 		}
 		
-		else if(userRes.getGuaranteed()==0){
-
-			if (cust.getCCExpiration() == instr[3] && cust.getCCNumber() == instr[4]){
-				userRes.setStatus(2);	
+		// if credit type is not given then check if reservation is guaranteed and compare credit information to the reservation
+		else if(instr.length == 4 && userRes.getGuaranteed()==0 && cust.getCCExpiration() == instr[instr.length - 2] && cust.getCCNumber() == instr[instr.length - 1])
 				checkedIn = true;
-			}
-		}
+		
+		// finish checking in the customer if they have provided sufficient information
 		if (checkedIn) {
+			userRes.setStatus(2); // change status of reservation to checked in
 			//assign room# to res, 0-MAX_singles is singles and max singles-max doubles is doubles
 			int roomNum= Rooms.getEmpty(userRes.getRoomType());
 			Rooms.fillRoom(roomNum);
 			userRes.setRoomNumber(roomNum);
 		}
+		
+		// OUTPUT GOES HERE
 	}
 
 }
