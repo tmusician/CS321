12161c6901795f67dfd65ffd7904ada83899b57c
tmusician <teliopou@gmu.edu>
2016-04-11 15:48:32 -0400

    Update Proxy.java

--- Assignment4/SoftwareArchitecturalDesign/src/Proxy.java
+++ Assignment4/SoftwareArchitecturalDesign/src/Proxy.java
@@ -1,8 +1,12 @@
 public class Proxy {
 
 	public static void run(String[] args) {
-		// TODO Auto-generated method stub
-
+		//initialize variables
+		int num = args[0];
+		int expDate = args[1];
+		
+		// if odd return false, else return true
+		return num % 2 != 0;
 	}
 
 }
