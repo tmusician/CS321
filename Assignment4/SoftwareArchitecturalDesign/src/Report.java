cd5bb38b177912aec09f1bb85490787d6fae32e1
KrishnaKRGit <krishnakrdms@gmail.com>
2016-04-11 16:28:21 -0400

    Update Report.java

--- Assignment4/SoftwareArchitecturalDesign/src/Report.java
+++ Assignment4/SoftwareArchitecturalDesign/src/Report.java
@@ -1,1 +1,19 @@
-
+public static Report{
+  
+  private int day;
+  private int income;
+  private int Occupancy;
+  
+  public static void main(String[] args){
+    
+    
+  }
+  
+  public static void incOccupancy(){ this.Occupancy++; }
+  
+  public static void incIncome(int inc){ this.income += inc; }
+  
+  //day change.
+  
+  
+}
