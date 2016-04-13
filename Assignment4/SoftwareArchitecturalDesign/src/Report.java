39d7daa4c6b04660a633a8815990d8904a6bb293
KrishnaKRGit <krishnakrdms@gmail.com>
2016-04-13 18:25:05 -0400

    Update Report.java

--- Assignment4/SoftwareArchitecturalDesign/src/Report.java
+++ Assignment4/SoftwareArchitecturalDesign/src/Report.java
@@ -1,16 +1,20 @@
 public static Report{
   
   private static int income;
-  private static int Occupancy;
+  private static int SR_Occupancy;
+  private static int DR_Occupancy;
   
   public static void main(String[] args){
     this.income = 0;
-    this.Occupancy = 0;
+    this.SR_Occupancy = 0;
+    this.DR_Occupancy = 0;
   }
   
-  public static void incOccupancy(){ this.Occupancy++; }
+  public static void inc_SR_Occupancy(){ this.SR_Occupancy++; }
+  public static void dec_SR_Occupancy(){ this.SR_Occupancy--; }
   
-  public static void decOccupancy(){ this.Occupancy--; }
+  public static void inc_DR_Occupancy(){ this.DR_Occupancy--; }
+  public static void dec_DR_Occupancy(){ this.DR_Occupancy--; }
   
   public static void incIncome(int inc){ this.income += inc; }
   
