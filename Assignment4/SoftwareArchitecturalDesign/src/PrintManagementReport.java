0824e821935b633f2077246f0d1f29fe1c6e3db6
KrishnaKRGit <krishnakrdms@gmail.com>
2016-04-14 18:01:34 -0400

    .
    .

--- Assignment4/SoftwareArchitecturalDesign/src/PrintManagementReport.java
+++ Assignment4/SoftwareArchitecturalDesign/src/PrintManagementReport.java
@@ -1,27 +1,7 @@
 
 public class PrintManagementReport {
 	
-	int date = args[1];
-		//compare date against current day.
-	
-	public static String makeReport_curDay(String[] args) {
-	
+	public static String viewReport(String input){
 		
-		
-		Day cur = Calendar.getInfo();      //need to get DATE!
-		
-		int num_SR_reserved = Framework.NUM_SINGLE_ROOMS - cur.getSingAvail();
-		int num_DR_reserved = Framework.NUM_DOUBLE_ROOMS - cur.getDoubAvail();
-		
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append("Management Report for 1/"+Calendar.get_cur_Day()+"/2016:\n");                                       //Add DATE here!!!!!!
-		sb.append("Number of Reservations: "+(num_SR_reserved+num_DR_reserved)+"\n");
-		sb.append("Single Rooms Reserved: "+ num_SR_reserved +"\n");
-		sb.append("Double Rooms Reserved: "+ num_DR_reserved +"\n");
-		sb.append("Occupancy Rate:"+int((Report.get_Occupancy/((double)(Framework.NUM_SINGLE_ROOMS+Framework.NUM_DOUBLE_ROOMS)))*100)+"\n");
-		sb.append("Total Revenue: $"+String.format("%0.2d",(double)report.get_income())+"\n");
-		
-		return sb.toString();
 	}
 }
--- Assignment4/SoftwareArchitecturalDesign/src/Report.java
+++ Assignment4/SoftwareArchitecturalDesign/src/Report.java
@@ -1,11 +1,14 @@
+import java.util.*;
 public static Report{
   
   private static int income;
   private static int Occupancy;
+  private HashMap<Integer,String> History;
   
   public static void main(String[] args){
     this.income = 0;
     this.Occupancy = 0;
+	this.History = new HashMap<Integer,String>();
   }
   
   public static void inc_Occupancy(){ this.Occupancy++; }
@@ -18,8 +21,28 @@ public static Report{
   public static int get_income(){ return this.income; }
   
   public static void day_change(){
+	this.History.put(Calendar.get_cur_Day,makeReport_curDay())
     this.income = 0;
   }
+  
+  public static String makeReport_curDay() {
+	
+	Day cur = Calendar.getInfo();      //need to get DATE!
+	
+	int num_SR_reserved = Framework.NUM_SINGLE_ROOMS - cur.getSingAvail();
+	int num_DR_reserved = Framework.NUM_DOUBLE_ROOMS - cur.getDoubAvail();
+	
+	StringBuilder sb = new StringBuilder();
+	
+	sb.append("Management Report for 1/"+Calendar.get_cur_Day()+"/2016:\n");                                       //Add DATE here!!!!!!
+	sb.append("Number of Reservations: "+(num_SR_reserved+num_DR_reserved)+"\n");
+	sb.append("Single Rooms Reserved: "+ num_SR_reserved +"\n");
+	sb.append("Double Rooms Reserved: "+ num_DR_reserved +"\n");
+	sb.append("Occupancy Rate:"+int((this.Occupancy/((double)(Framework.NUM_SINGLE_ROOMS+Framework.NUM_DOUBLE_ROOMS)))*100)+"\n");
+	sb.append("Total Revenue: $"+String.format("%0.2d",(double)this.get_income())+"\n");
+	
+	return sb.toString();
+}
   
   
   
