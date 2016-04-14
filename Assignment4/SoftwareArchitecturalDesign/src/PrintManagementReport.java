ad4b1e6bb23180e8dab837aa67fdf52fb5a46cd8
KrishnaKRGit <krishnakrdms@gmail.com>
2016-04-13 22:51:06 -0400

    Update PrintManagementReport.java

--- Assignment4/SoftwareArchitecturalDesign/src/PrintManagementReport.java
+++ Assignment4/SoftwareArchitecturalDesign/src/PrintManagementReport.java
@@ -13,5 +13,13 @@ public class PrintManagementReport {
 		
 		StringBuilder sb = new StringBuilder();
 		
+		sb.append("Management Report for day:   \n");                                       //Add DATE here!!!!!!
+		sb.append("Number of Reservations: "+(num_SR_reserved+num_DR_reserved)+"\n");
+		sb.append("Single Rooms Reserved: "+ num_SR_reserved +"\n");
+		sb.append("Double Rooms Reserved: "+ num_DR_reserved +"\n");
+		sb.append("Occupancy Rate:"+int((Report.get_Occupancy/((double)(Framework.NUM_SINGLE_ROOMS+Framework.NUM_DOUBLE_ROOMS)))*100)+"\n");
+		sb.append("Total Revenue: $"+String.format("%0.2d",(double)report.get_income())+"\n");
+		
+		return sb.toString();
 	}
 }
