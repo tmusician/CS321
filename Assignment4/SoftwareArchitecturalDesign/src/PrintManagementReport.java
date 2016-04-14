38002f07bbd2c869ec3246157f518f0bf9deeb0a
KrishnaKRGit <krishnakrdms@gmail.com>
2016-04-13 20:24:58 -0400

    Update PrintManagementReport.java

--- Assignment4/SoftwareArchitecturalDesign/src/PrintManagementReport.java
+++ Assignment4/SoftwareArchitecturalDesign/src/PrintManagementReport.java
@@ -2,6 +2,16 @@
 public class PrintManagementReport {
 	
 	public static String viewReport(String[] args) {
-		int SR_reserved = 
+	
+		int date = args[1];
+		//compare date against current day.
+		
+		Day cur = Calendar.getInfo();      //need to get DATE!
+		
+		int num_SR_reserved = Framework.NUM_SINGLE_ROOMS - cur.getSingAvail();
+		int num_DR_reserved = Framework.NUM_DOUBLE_ROOMS - cur.getDoubAvail();
+		
+		StringBuilder sb = new StringBuilder();
+		
 	}
 }
