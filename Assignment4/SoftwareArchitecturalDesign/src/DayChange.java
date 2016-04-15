56f1675a5f378de5497b5c2010d555f75d0d93bc
KrishnaKRGit <krishnakrdms@gmail.com>
2016-04-15 00:42:01 -0400

    .
    .

--- Assignment4/SoftwareArchitecturalDesign/src/DayChange.java
+++ Assignment4/SoftwareArchitecturalDesign/src/DayChange.java
@@ -1,28 +1,28 @@
-
+import java.util.*;
 public class DayChange {
 
-	public static String change_day(String[] args) {
-		
-		StringBuilder op = new StringBuilder();
-		
-		Day today = Calendar.getInfo();
-		int[] cur_reservs = today.getResIDs();
-		
-		for(int i=0; i<cur_reservs.length; i++){
-			Reservation temp = Framework.getReservationByID(cur_reservs[i]);
-			if((temp.getGuaranteed()==1)&&((temp.getStatus()!=Framework.STATUS_CHECKED_IN)&&(temp.getStatus()!=Framework.STATUS_CHECKED_OUT))){
-				//no show condition.
-				Customer no_show = Framework.getCustomerByID(temp.getCustomerID());
-				op.append(no_show.getName()+" did not show.\n");
-				temp.setStatus(Framework.STATUS_NO_SHOW);
-			} continue;
-		}
-		
-		Report.day_change();
-		Calendar.nextDay();
-		op.append("==================January "+Calendar.get_cur_Day()+", 2016==================\n");
-		
-		return op.toString();
-	}
+ public static String change_day(String[] args) {
+  
+  StringBuilder op = new StringBuilder();
+  
+  Day today = Calendar.getInfo();
+  ArrayList<Integer> cur_reservs = today.getResIDs();
+  
+  for(int i=0; i<cur_reservs.size(); i++){
+   Reservation temp = Framework.getReservationByID(cur_reservs.get(i));
+   if((temp.getGuaranteed()==1)&&((temp.getStatus()!=Framework.STATUS_CHECKED_IN)&&(temp.getStatus()!=Framework.STATUS_CHECKED_OUT))){
+    //no show condition.
+    Customer no_show = Framework.getCustomerByID(temp.getCustomerID());
+    op.append(no_show.getName()+" did not show.\n");
+    temp.setStatus(Framework.STATUS_NO_SHOW);
+   } continue;
+  }
+  
+  Report.day_change();
+  Calendar.nextDay();
+  op.append("==================January "+Calendar.get_cur_Day()+", 2016==================\n");
+  
+  return op.toString();
+ }
 
 }
--- Assignment4/SoftwareArchitecturalDesign/src/SixPmAlarm.java
+++ Assignment4/SoftwareArchitecturalDesign/src/SixPmAlarm.java
@@ -1,17 +1,17 @@
-
+import java.util.*;
 public class SixPmAlarm {
 
  public static String run(String[] args) {
   
   StringBuilder pm = new StringBuilder();
   pm.append("6PM Signal:\n");
-  int[] reservations = (Calendar.getInfo()).getResIDs();
+  ArrayList<Integer> reservations = (Calendar.getInfo()).getResIDs();
   
-  for(int i=0; i<reservations.length; i++){
-   Reservation temp = Framework.getReservationByID(reservations[i]);
+  for(int i=0; i<reservations.size(); i++){
+   Reservation temp = Framework.getReservationByID(reservations.get(i));
    if (temp.getGuaranteed()==0){
     pm.append("Cancelled Reservation for "+((Framework.getCustomerByID(temp.getCustomerID())).getName())+"\n");
-    Framework.deleteReservation(reservations[i]);
+    Framework.deleteReservation(reservations.get(i));
     (Calendar.getInfo()).removeReservation(temp.getReservationID());
    }continue;
   }
