c01326f9f44b28f182ae0226feee2e93a3ebe1c9
KrishnaKRGit <krishnakrdms@gmail.com>
2016-04-15 01:27:10 -0400

    .

--- Assignment4/SoftwareArchitecturalDesign/src/Report.java
+++ Assignment4/SoftwareArchitecturalDesign/src/Report.java
@@ -1,14 +1,51 @@
 /* CLASS INTERFACE SPECIFICATION: 
+ * 
  * Information Hiding Class : Report:
  * Author: Krishna Kanakapura.
+ * 
  * Operations Provided:
- *  -increment and decrement total Occupancy of the Hotel.
- *  -makeReport_curDay():
- *   -function: create and store the management report for today.
- *   -precondition:System is functional and running.
- *   -postcondition:A report has been created successfully.
- *   -input parameters: int date.
- *   -output parameters: String report.
+ * 
+ * -inc_Occupancy:
+ *    -function: increment the hotels total occupancy.
+ *    -precondition: Systm is initiated.
+ *    -postcondition: hotels occupancy has been incremented.
+ *    -input parameters: none.
+ *    -output parameters: none.
+ * 
+ * -dec_Occupancy:
+ *    -function: decrement the hotels total occupancy.
+ *    -precondition: Systm is initiated.
+ *    -postcondition: hotels occupancy has been decremented.
+ *    -input parameters: none.
+ *    -output parameters: none.
+ * 
+ * -incIncome:
+ *    -function: increment the hotels revenue by the given amount.
+ *    -precondition: System is initiated.
+ *    -postcondition: the hotels revenue is incremented by the given amount.
+ *    -input paramenters: int amount.
+ *    -output paramenters: none.
+ * 
+ * -day_change():
+ *    -function: creates todays report and resets the static int income.
+ *    -precondition: system is initiated.
+ *    -postcondition: todays report is created and stored.
+ *    -input parameters:none.
+ *    -output parameters: none.
+ * 
+ * -get_report(date):
+ *    -function: return previous day(s) reports.
+ *    -precondition: system has been running for at least one complete day.
+ *    -postconditon: a previous report is returned.
+ *    -input parameters: int date.
+ *    -output parameters: String report.
+ * 
+ * -makeReport_curDay():
+ *    -function: create and store the management report for today.
+ *    -precondition: System is functional and running.
+ *    -postcondition: A report has been created successfully.
+ *    -input parameters: int date.
+ *    -output parameters: String report.
  */
 import java.util.*;
 public class Report{
