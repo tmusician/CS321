fa25813e524cff3fad80552cdde91cc0550d7962
KrishnaKRGit <krishnakrdms@gmail.com>
2016-04-14 23:05:34 -0400

    bugs

--- Assignment4/SoftwareArchitecturalDesign/src/Proxy.java
+++ Assignment4/SoftwareArchitecturalDesign/src/Proxy.java
@@ -1,10 +1,10 @@
 public class Proxy {
-	public static boolean validateCard(String[] args) {
-		//initialize variables
-		int num = args[0];
-		int expDate = args[1];
-		
-		// if credit card number is odd then return false, else return true
-		return num % 2 != 0;
-	}
+ public static boolean validateCard(String[] args) {
+  //initialize variables
+  int num = Integer.parseInt(args[0]);
+  int expDate = Integer.parseInt(args[1]);
+  
+  // if credit card number is odd then return false, else return true
+  return num % 2 != 0;
+ }
 }
--- Assignment4/SoftwareArchitecturalDesign/src/Report.java
+++ Assignment4/SoftwareArchitecturalDesign/src/Report.java
@@ -3,49 +3,49 @@ public class Report{
   
   private static int income;
   private static int Occupancy;
-  private HashMap<Integer,String> History;
+  private static HashMap<Integer,String> History;
   
   public static void main(String[] args){
-    this.income = 0;
-    this.Occupancy = 0;
-	this.History = new HashMap<Integer,String>();
+    income = 0;
+    Occupancy = 0;
+    History = new HashMap<Integer,String>();
   }
   
-  public static void inc_Occupancy(){ this.Occupancy++; }
-  public static void dec_Occupancy(){ this.Occupancy--; }
+  public static void inc_Occupancy(){ Occupancy++; }
+  public static void dec_Occupancy(){ Occupancy--; }
   
-  public static void incIncome(int inc){ this.income += inc; }
+  public static void incIncome(int inc){ income += inc; }
   
-  public static int get_Occupancy(){ return this.Occupancy; }
+  public static int get_Occupancy(){ return Occupancy; }
   
-  public static int get_income(){ return this.income; }
+  public static int get_income(){ return income; }
   
   public static void day_change(){
-	this.History.put(Calendar.get_cur_Day,makeReport_curDay());
-    this.income = 0;
+    History.put(Calendar.get_cur_Day(),makeReport_curDay());
+    income = 0;
   }
   
   public static String makeReport_curDay() {
-	
-	Day cur = Calendar.getInfo();      
-	
-	int num_SR_reserved = Framework.NUM_SINGLE_ROOMS - cur.getSingAvail();
-	int num_DR_reserved = Framework.NUM_DOUBLE_ROOMS - cur.getDoubAvail();
-	
-	StringBuilder sb = new StringBuilder();
-	
-	sb.append("Management Report for 1/"+Calendar.get_cur_Day()+"/2016:\n");                                       
-	sb.append("Number of Reservations: "+(num_SR_reserved+num_DR_reserved)+"\n");
-	sb.append("Single Rooms Reserved: "+ num_SR_reserved +"\n");
-	sb.append("Double Rooms Reserved: "+ num_DR_reserved +"\n");
-	sb.append("Occupancy Rate:"+String.format("%2d",((this.Occupancy/((double)(Framework.NUM_SINGLE_ROOMS+Framework.NUM_DOUBLE_ROOMS)))*100))+"\n");
-	sb.append("Total Revenue: $"+String.format("%0.2d",(double)this.get_income())+"\n");
-	
-	return sb.toString();
-}
-
-	public static String get_report(int date){
-		return this.History.get(date);
-	}
+    
+    Day cur = Calendar.getInfo();      
+    
+    int num_SR_reserved = Framework.NUM_SINGLE_ROOMS - cur.getSingAvail();
+    int num_DR_reserved = Framework.NUM_DOUBLE_ROOMS - cur.getDoubAvail();
+    
+    StringBuilder sb = new StringBuilder();
+    
+    sb.append("Management Report for 1/"+Calendar.get_cur_Day()+"/2016:\n");                                       
+    sb.append("Number of Reservations: "+(num_SR_reserved+num_DR_reserved)+"\n");
+    sb.append("Single Rooms Reserved: "+ num_SR_reserved +"\n");
+    sb.append("Double Rooms Reserved: "+ num_DR_reserved +"\n");
+    sb.append("Occupancy Rate:"+String.format("%2d",((Occupancy/((double)(Framework.NUM_SINGLE_ROOMS+Framework.NUM_DOUBLE_ROOMS)))*100))+"\n");
+    sb.append("Total Revenue: $"+String.format("%0.2d",(double)get_income())+"\n");
+    
+    return sb.toString();
+  }
+  
+  public static String get_report(int date){
+    return History.get(date);
+  }
   
 }
